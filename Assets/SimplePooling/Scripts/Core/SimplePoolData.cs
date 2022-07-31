6f4b1e8f59976bd36e0e5daae066fee250808e62
Ahmet Omak <ahmetaltioglu@hotmail.com>
2022-07-31 17:10:37 +0300

    chore: SimplePoolData Updated

--- Assets/SimplePooling/Scripts/Core/SimplePoolData.cs
+++ Assets/SimplePooling/Scripts/Core/SimplePoolData.cs
@@ -5,11 +5,11 @@ namespace SimplePooling
     [CreateAssetMenu(fileName = "Simple Pool", menuName = "Simple Pooling/Simple Pool")]
     public class SimplePoolData : ScriptableObject
     {
-        [SerializeField] GameObject prefab;
-        [SerializeField] int startSize;
-        [SerializeField] int refillSize;
-        [SerializeField] string parentName;
-        [SerializeField] string selfName;
+        [SerializeField, Space(2f)] GameObject prefab;
+        [SerializeField, Space(2f)] int startSize;
+        [SerializeField, Space(2f)] int refillSize;
+        [SerializeField, Space(2f)] string parentName;
+        [SerializeField, Space(2f)] string selfName;
 
         public GameObject Prefab { get => prefab; }
         public int StartSize { get => startSize; }
